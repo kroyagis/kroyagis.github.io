8e38c69d013c8c812883bec607ded618436ed29d
Ji You <kroyagis@gmail.com>
2016-08-23 13:19:06 -0400

    -added throttling and debounce for optimal performance -added correct value for the center of eyeball

--- javascript/jiyou.js
+++ javascript/jiyou.js
@@ -1,5 +1,5 @@
 function throttle(fn, threshhold, scope) {
-  threshhold || (threshhold = 51);
+  threshhold || (threshhold = 100);
   var last,
       deferTimer;
   return function () {
@@ -21,66 +21,127 @@ function throttle(fn, threshhold, scope) {
   };
 };
 
-var DrawEye = function(eyecontainer, pupil, eyeposx, eyeposy){
-  var r = document.querySelector(pupil).offsetWidth/2;
-  var center = {
-    x: document.querySelector(eyecontainer).offsetWidth/2 - r,
-    y: document.querySelector(eyecontainer).offsetHeight/2 - r
+function debounce(fn, delay) {
+  var timer = null;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
   };
-  var distanceThreshold = document.querySelector(eyecontainer).offsetWidth/2 - r;
-  var mouseX = 0, mouseY = 0;
-  //Mouse movement
-  document.addEventListener('mousemove', throttle(function(e){
-    var d = {
-      x: e.pageX - r - eyeposx - center.x,
-      y: e.pageY - r - eyeposy - center.y
-    };
-    var distance = Math.sqrt(d.x*d.x + d.y*d.y);
-    if (distance < distanceThreshold) {
-      mouseX = e.pageX - eyeposx - r;
-      mouseY = e.pageY - eyeposy - r;
-    } else {
-      mouseX = d.x / distance * distanceThreshold + center.x;
-      mouseY = d.y / distance * distanceThreshold + center.y;
-    }
-  }));
+}
 
-  // Update pupil location
-  var pupil = document.querySelector(pupil);
-  var xp = 0, yp = 0;
-  var loop = setInterval(function(){
-    // change 1 to alter damping/momentum - higher is slower
-    xp += (mouseX - xp) / 1;
-    yp += (mouseY - yp) / 1;
-    pupil.style.left = xp+"px";
-    pupil.style.top = yp+"px";
-  }, 1);
-};
-var w = window;
-var avatareye1 = new DrawEye(".eyeleft", ".pupilleft", (w.innerWidth/2 + 52.0625), (w.innerHeight - 97.595));
-var avatareye2 = new DrawEye(".eyeright", ".pupilright", (w.innerWidth/2 - 84.0625), (w.innerHeight - 95.595));
+function Xeyes(faceClass, e1Class, e1Lft, e1Top, e1Radius, e2Class, e2Lft, e2Top, e2Radius)
+{
+  var faceObj = document.querySelector(faceClass),
+      eye1Obj = document.querySelector(e1Class),
+      eye2Obj = document.querySelector(e2Class),
+      e1x, e1y,    // eye centre relative to top left of doc
+      r1,
+      e1xLoc, e1yLoc,   // eye top left relative to top left of parent
+      e2x, e2y,
+      r2,          // eye radii
+      e2xLoc, e2yLoc;
 
+  // setup initial eye locations
+  var eyesInit = debounce(function()
+  {
+    var faceWidth = faceObj.offsetWidth,
+        faceHeight = faceObj.offsetHeight,
+        tmp;
 
-var getPosition = function(el){
-  var xPos = 0;
-  var yPos = 0;
+    function getPosition(el) {
+      var xPos = 0;
+      var yPos = 0;
 
-  while (el){
-    if (el.tagName = "BODY"){
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+      while (el) {
+        if (el.tagName == "BODY") {
+          // deal with browser quirks with body/window/document and page scroll
+          var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+          var yScroll = el.scrollTop || document.documentElement.scrollTop;
 
-      xPos += (el.offsetLeft - xScroll + el.clientLeft);
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+          xPos += (el.offsetLeft - xScroll + el.clientLeft);
+          yPos += (el.offsetTop - yScroll + el.clientTop);
+        } else {
+          // for all other non-BODY elements
+          xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+          yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+        }
+
+        el = el.offsetParent;
+      }
+      return {
+        x: xPos,
+        y: yPos
+      };
     }
 
-    el = el.offsetParent;
-  }
-  return {
-    x: xPos,
-    y: yPos
-  };
+    // get left,top of eyes relative to parent
+    e1xLoc = 0.01*e1Lft*faceWidth - eye1Obj.offsetWidth/2;
+    e1yLoc = 0.01*e1Top*faceHeight - eye1Obj.offsetHeight/2;
+    e2xLoc = 0.01*e2Lft*faceWidth - eye2Obj.offsetWidth/2;
+    e2yLoc = 0.01*e2Top*faceHeight - eye2Obj.offsetHeight/2;
+
+    // get absolute position of centre of eyes
+    e1x = window.innerWidth/2 + 60;
+    e1y = window.innerHeight - 86;
+    e2x = window.innerWidth/2 - 60;
+    e2y = window.innerHeight - 86;
+
+    r1 = 0.01*e1Radius*faceWidth;
+    r2 = 0.01*e2Radius*faceWidth;
+    // now move the eyes to a less goggle-eye position until mouse moves
+    eye1Obj.style.left = e1xLoc+"px";        // "12.4em";
+    eye1Obj.style.top = e1yLoc+r1/1.5+"px";    // "16.3em";
+    eye2Obj.style.left = e2xLoc+"px";        // "21.0em";
+    eye2Obj.style.top = e2yLoc+r2/1.5+"px";    // "16.3em";
+
+    console.log("Eyes have been initialized");
+  }, 250);
+
+  var moveEyes = throttle(function(e){
+    var csrX, csrY,
+        x, y,
+        dx, dy,
+        R, d;
+
+    if (e.pageX)
+    {
+      csrX = e.pageX;
+      csrY = e.pageY;
+    }
+    else
+    {
+      // IE case
+      d = (document.documentElement && document.documentElement.scrollLeft != null) ?
+             document.documentElement : document.body;
+      csrX = e.clientX + d.scrollLeft;
+      csrY = e.clientY + d.scrollTop;
+    }
+    // eye 1 first
+    dx = csrX - e1x;
+    dy = csrY - e1y;
+    R = Math.sqrt(dx*dx+dy*dy);     // distance from eye centre to csr
+    x = (R < r1)? dx : dx*r1/R;
+    y = (R < r1)? dy : dy*r1/R;
+    eye1Obj.style.left = x + e1xLoc + "px";
+    eye1Obj.style.top = y + e1yLoc + "px";
+    // now for eye 2
+    dx = csrX - e2x;
+    dy = csrY - e2y;
+    R = Math.sqrt(dx*dx+dy*dy);
+    x = (R < r2)? dx : dx*r2/R;
+    y = (R < r2)? dy : dy*r2/R;
+    eye2Obj.style.left = x + e2xLoc + "px";
+    eye2Obj.style.top = y + e2yLoc + "px";
+    console.log(e.pageX, e.pageY);
+  });
+
+  eyesInit();
+  // if the browser window is resized eye locations must be re-calculated
+  addEvent(window, "resize", eyesInit);
+  // setup handler for cursor move
+  addEvent(document, "mousemove", moveEyes);
+  // for debug use     document.onclick = moveEyes;
 }
